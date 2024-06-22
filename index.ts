7252e6a1e35c0c362bf7f1ec6fd240a1309ccb28
KhulaJaved <khulajaved1995@gmail.com>
2024-06-22 14:50:11 +0500

    created word counter

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answers:{
+    Sentence: string
+} = await inquirer.prompt([
+    {
+        name:"Sentence",
+        type:"input",
+        message:"Entr your sentence to count the word:"   
+        }
+])
+
+const words = answers.Sentence.trim().split(" ")
+
+console.log(words)
+
+console.log(`Your sentence word count is ${words.length}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
